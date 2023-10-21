b2648647e9647577c9760fc17eec466f902b0b16
ricekeller <ricekeller@gmail.com>
2023-10-21 12:58:40 -0400

    add pages

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
   socials: {
-    twitter: 'Atinux',
-    github: 'Atinux/content-wind'
+    twitter: '',
+    github: 'ricekeller'
   }
 })

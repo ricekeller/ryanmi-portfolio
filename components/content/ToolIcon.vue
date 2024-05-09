36b285a6a6bb823e5e54198d3b61d6543c0903d6
ricekeller <ricekeller@gmail.com>
2024-05-09 17:13:34 +0000

    add ToolIcon component

---
+++ components/content/ToolIcon.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    default: "",
+  }
+})
+</script>
+
+<template>
+  <img class="tool-icon" :src="icon" :title="title" />
+</template>
+
+<style scoped>
+.tool-icon
+{
+    width: 40px;
+    height: 40px;
+    display: inline-block;
+}
+</style>
